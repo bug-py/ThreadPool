ab5380ca8147aeed33d71c52f6cc209150c638c3
bug-py <cocorigolo6@gmail.com>
2026-03-21 23:45:08 +0100

    correction

--- src/queue.c
+++ src/queue.c
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 
 void QUEUE_init(queue_t* queue,size_t element_size,size_t init_capacity){
-        if(!init_size) init_size=1;
+        if(!init_capacity) init_capacity=1;
         if(!element_size) element_size=1;
-        queue->array=safe_alloc(element_size,init_size,NULL);
-        queue->capacity=init_size;
+        queue->array=safe_alloc(element_size,init_capacity,NULL);
+        queue->capacity=init_capacity;
         queue->length=0;
         queue->element_size=element_size;
 }
